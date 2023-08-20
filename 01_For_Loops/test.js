94f8aba05cf28a4a0111b65cace42f7ccf14d0c4
Aniket Chauhan <132349103+aniketchauhan18@users.noreply.github.com>
2023-08-20 05:37:05 +0000

    comming config

--- 01_For_Loops/test.js
+++ 01_For_Loops/test.js
@@ -1,1 +1,93 @@
-console.log("Aniket Chauhan");
+let people = ["Aniket", "Chauhan", "Blabla", "Hello"];
+
+// ****************Traditional For Loops for iteration in array*********************
+// for (let index = 0; index < people.length; index++) {
+//     const element = people[index];
+//     console.log(element);
+// }
+
+
+let obj = {
+    name: "Aniket",
+    language: "JavaScript",
+    hobbies: "Web App Development",
+}
+// console.log(obj);
+// 1.1 Iterating Objects using traditional for loop;
+// for (let index = 0; index < Object.keys(obj).length; index++) {
+//     const element = obj[Object.keys(obj)[index]]; // object.keys(obj) shows all the content inside obj
+//     console.log(element);
+// }
+
+
+// 1.2 Iterating an object using for in loop
+for (const key in obj) {
+    // console.log(key); // show all the properties of the object like name, language etc
+    // console.log(obj[key]); //  show all the values of the object properties
+    // console.log(`${key} :- ${obj[key]}`) // Show all both properties and values
+}
+
+
+let object = {
+    title: 'Harry Potter',
+    anotherObject: {
+        Name: 'Noob',
+        Experience: 'No Exp!!'
+    },
+    schooling: 'xyz school'
+}
+
+for (const key in object) {
+    if (typeof object[key] === 'object') {
+        for (const innerKey in object[key]) { // This is to log nested object inside an object
+            // console.log(object[key][innerKey])
+        }
+    } else {
+        // console.log(object[key])
+    }
+    
+};
+
+//  1.3 We can use for in loop to loop through each character
+
+const myString = "Aniket Chauhan";
+
+for (const char in myString) {
+    // console.log(myString[char]);
+}
+
+// ****************For In Loop in Array****************
+
+let array = [
+    "Aniket",
+    "Chauhan",
+    "Nitin",
+    "Kapoor"
+];
+// 1.4 Iterationg an array using for in loop
+for (const key in array) {
+    // console.log(array[key]);
+}
+
+//********************* For Of Loop ******************
+
+let newPeople = [
+    "Aniket",
+    "Chauhan",
+    "Blabla",
+    [
+        "this is child array",
+        "inside parent array"
+    ]
+];
+
+for (const name of newPeople) {
+    if(typeof name === 'array'){
+        for (const innerName of name) {
+            console.log(innerName)
+        }
+    } else {
+        console.log(name); // will directly log the values of array    
+    }
+    
+}
